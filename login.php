1f2124331a51c10b4204e08cfed4b5271aaac4c5
Hung Nguyen <hungcqnguyen@gmail.com>
2024-03-28 14:49:52 -0400

    Adding session

--- login.php
+++ login.php
@@ -1,3 +1,18 @@
+<?php
+    session_start();
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $username = $_POST["logInName"];
+                    $password = $_POST["logInPassword"];
+                    if ($username == "Test" and $password == "1234") {
+                        $_SESSION["access"] = 1;
+                        $_SESSION["username"] = $username;
+                        header('Location: main_menu.php');
+                        exit;
+                    } else {
+                        echo "Incorrect Password or Username";
+                    }
+                }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,7 +27,7 @@
         <div class="selection">
             <fieldset class="form">
                 <legend>Login</legend>
-                <form method="get" action="./main_menu.php">
+                <form method="post" action="login.php">
                     <label for="logInName">Enter your Username:</label><br>
                     <input type="text" id="logInName" name="logInName" size="60"><br><br>
                     <label for="logInPassword">Enter your Password:</label><br>
